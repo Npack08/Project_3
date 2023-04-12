cc17788f0679b541116dcc9b2cc9fad4b0ec0282
Jakesb27 <jake.boden27@gmail.com>
2023-04-11 20:28:38 -0700

    misc.

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # # Read sqlite query results into a pandas DataFrame
-# conn = sqlite3.connect("heart.db")
+# conn = sqlite3.connect("resources/heart.db")
 # df = pd.read_sql_query("SELECT * FROM heart_db", conn)
-
+#
 # # Close connection
 # conn.close()
 
+
 @app.route('/')
 def index():
+
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
