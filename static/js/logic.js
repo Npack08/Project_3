1a1c6d5e8dbecfd1bafffdda23a221ba3b3ec309
Justine <115434763+JustinePile@users.noreply.github.com>
2023-04-13 14:56:24 -0600

    Updated code Changes to be committed: 	modified:   static/js/logic.js 	modified:   templates/index.html

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
-const url = 'http://127.0.0.1:5000/api/data';
+const URL = 'http://127.0.0.1:5000/api/data';
 
 // Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
+d3.json(URL).then(function(data) {
   console.log(data);
 
   // Group data by ageGroup
