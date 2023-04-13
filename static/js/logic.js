f6be37345531da6500bd9240e80cc06c34383e22
Justine <115434763+JustinePile@users.noreply.github.com>
2023-04-13 13:53:49 -0600

    Created plot3 and updated HTML so that each plot will be in its own row and full container width Changes to be committed: 	modified:   static/css/style.css 	modified:   static/js/logic.js 	modified:   templates/index.html

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,43 @@
 const url = 'http://127.0.0.1:5000/api/data';
 
-// Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
-
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
   console.log(data);
+
+  // Group data by ageGroup
+  const groupedData = d3.group(data, d => d.ageGroup);
+
+  // Loop through the grouped data and create plot3 traces for each ageGroup
+  const traces_plot3 = Array.from(groupedData, ([ageGroup, groupData]) => {
+    const restingECG = groupData.map(obj => obj.restingECG);
+    const maxHR = groupData.map(obj => obj.maxHR);
+
+    return {
+      x: restingECG,
+      y: maxHR,
+      mode: 'markers',
+      type: 'bar',
+      name: ageGroup
+    };
+  });
+
+  // Create plot3 layout object
+  const layout_plot3 = {
+    title: 'Resting ECG vs Max Heart Rate',
+    xaxis: {
+      title: 'Resting ECG'
+    },
+    yaxis: {
+      title: 'Max Heart Rate'
+    },
+    legend : {
+      title: {
+        text: "   Age groups"
+      }
+    },
+    barmode: 'group'
+  };
+
+  // Create plot on plot3 div
+  Plotly.newPlot('plot3', traces_plot3, layout_plot3);
 });
