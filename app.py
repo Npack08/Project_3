83a7613be1fd412cba0c99cd1b8bb0bfadf49e18
Jakesb27 <jake.boden27@gmail.com>
2023-04-16 17:24:18 -0700

    added functions to return data after agg it in pandas

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
+import pandas as pd
 import sqlite3
 
+
+conn = sqlite3.connect('resources/heart_db.db')
+heart_data = pd.read_sql_query("SELECT * FROM heart_data", conn)
+conn.close()
 
 app = Flask(__name__)
 
@@ -8,31 +13,39 @@ app = Flask(__name__)
 @app.route('/api/data')
 def api_data():
 
-    # create connection to database
-    conn = sqlite3.connect('resources/heart_db.db')
+    df = heart_data
+    df_dict = df.to_dict('records')
+    return jsonify(df_dict)
 
-    # create cursor object
-    cur = conn.cursor()
 
-    # execute query
-    cur.execute("SELECT * FROM heart_data")
+@app.route('/api/male_cholesterol')
+def male_chol_data():
 
-    # get column names
-    cols = [description[0] for description in cur.description]
+    df = heart_data
+    male_df = df.loc[df['sex'] == "M"].groupby('ageGroup').mean().round(2)
+    df_dict = male_df.to_dict('index')
 
-    # retrieve data
-    results = cur.fetchall()
+    return jsonify(df_dict)
 
-    # close connection
-    conn.close()
 
-    # zip column names and data into list of dictionary objects
-    data = []
-    for row in results:
-        data.append(dict(zip(cols, row)))
+@app.route('/api/female_cholesterol')
+def female_chol_data():
 
-    # return json data to api call
-    return jsonify(data)
+    df = heart_data
+    female_df = df.loc[df['sex'] == "F"].groupby('ageGroup').mean().round(2)
+    df_dict = female_df.to_dict('index')
+
+    return jsonify(df_dict)
+
+
+@app.route('/api/all_sex_cholesterol')
+def all_sex_chol_data():
+
+    df = heart_data
+    all_sex_df = df.groupby('ageGroup').mean().round(2)
+    df_dict = all_sex_df.to_dict('index')
+
+    return jsonify(df_dict)
 
 
 if __name__ == '__main__':
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,15 +1,67 @@
-const URL = 'http://127.0.0.1:5000/api/data';
+const ALL_DATA_URL = 'http://127.0.0.1:5000/api/data';
+const MALE_CHOL_URL = 'http://127.0.0.1:5000//api/male_cholesterol';
+const FEMALE_CHOL_URL = 'http://127.0.0.1:5000//api/female_cholesterol';
+const ALL_SEX_CHOL_URL = 'http://127.0.0.1:5000//api/all_sex_cholesterol';
 
 var AGE_SORT;
+var MALE_CHOL;
+var FEMALE_CHOL;
+var ALL_SEX_CHOL;
 
-// Fetch the JSON data and console log it
-d3.json(URL).then(function(data) {
+// Fetch the JSON data
+d3.json(ALL_DATA_URL).then(function(data) {
 
   AGE_SORT = data.sort((a, b) => d3.ascending(a.ageGroup, b.ageGroup))
 
   // invoke function for first graph with sorted data
   restingECG();
-  avgCholestrol();
+
+});
+
+// Fetch the JSON data
+d3.json(MALE_CHOL_URL).then(function(data) {
+
+  MALE_CHOL = data;
+
+});
+
+// Fetch the JSON data
+d3.json(FEMALE_CHOL_URL).then(function(data) {
+
+  FEMALE_CHOL = data;
+
+});
+
+// Fetch the JSON data
+d3.json(ALL_SEX_CHOL_URL).then(function(data) {
+
+  ALL_SEX_CHOL = data;
+
+  // invoke function for second graph with sorted data
+  let xValues = Object.keys(ALL_SEX_CHOL);
+  let yValues = Object.values(ALL_SEX_CHOL).map(d => d.cholesterol);
+
+  let trace1 = {
+    x: xValues,
+    y: yValues,
+    mode: 'markers',
+    type: 'bar',
+    name: yValues
+  };
+
+  // Create plot3 layout object
+  const layout1 = {
+    title: 'Average Cholesterol vs Age Group ',
+    xaxis: {
+      title: 'Age Group'
+    },
+    yaxis: {
+      title: 'Average Cholesterol'
+    },
+    barmode: 'group'
+  };
+
+  Plotly.newPlot('plot2', [trace1]);
 
 });
 
@@ -59,21 +111,31 @@ function restingECG() {
 //////////////////////////////////////////////
 //     Average Cholesterol vs Age Group     //
 //////////////////////////////////////////////
-function avgCholestrol() {
+d3.selectAll("#selDataset").on("change", updateAvgCholesterol);
 
-  let cholData = Array.from(d3.rollup(AGE_SORT, 
-    v => d3.mean(v, d => d.cholesterol), d => d.ageGroup));
-    
-  let xValues = cholData.map(d => d[0]);
-  let yValues = cholData.map(d => Number((d[1]).toFixed(2)));
+function updateAvgCholesterol() {
 
-  let trace1 = {
-    x: xValues,
-    y: yValues,
-    mode: 'markers',
-    type: 'bar',
-    name: yValues
+  let dropdownMenu = d3.select("#selDataset");
+  let dataset = dropdownMenu.property("value");
+  let xValues;
+  let yValues;
+
+  if (dataset == 'All Sexes') {
+    xValues = Object.keys(ALL_SEX_CHOL);
+    yValues = Object.values(ALL_SEX_CHOL).map(d => d.cholesterol);
+  }
+
+  else if (dataset == 'Male') {
+    xValues = Object.keys(MALE_CHOL);
+    yValues = Object.values(MALE_CHOL).map(d => d.cholesterol);
+    console.log(xValues);
+  }
+
+  else if (dataset == 'Female') {
+    xValues = Object.keys(FEMALE_CHOL);
+    yValues = Object.values(FEMALE_CHOL).map(d => d.cholesterol);
   };
 
-  Plotly.newPlot('plot2', [trace1]);
+  Plotly.restyle('plot2', "x", [xValues]);
+  Plotly.restyle('plot2', "y", [yValues]);
 };
