fb15f58f6bd9c06ac3c4abcaa9e1541bbcbc00c9
Justine <115434763+JustinePile@users.noreply.github.com>
2023-04-18 21:16:51 -0600

    Fix a few small things

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,51 +1,53 @@
 const URL = 'http://127.0.0.1:5000/api/data';
 
-var GROUPED_DATA;
-
 // Fetch the JSON data and console log it
 d3.json(URL).then(function(data) {
   // invoke function for first graph with sorted data
   groupType(data.sort((a, b) => d3.ascending(a.ageGroup, b.ageGroup)));
-
+  plot2Group(data);
 });
-const groupedData = d3.group(data, d => d.chestPainType);
 
-//Loop through the grouped data and create a plot trace
-const TRACES = Array.from(groupedData, ([chestPainType, groupedData]) => {
-  const CHOLESTEROL = d3.mean(groupedData, d => d.cholesterol);
+function plot2Group(data) {
+  let GROUPED_DATA = d3.group(data, d => d.chestPainType);
+  
+  //Loop through the grouped data and create a plot trace
+  const TRACES = Array.from(GROUPED_DATA, ([chestPainType, groupedData]) => {
+    const CHOLESTEROL = d3.mean(groupedData, d => d.cholesterol);
 
-  return{
-    x: [chestPainType],
-    y: [CHOLESTEROL],
-    type: 'bar',
-    name: chestPainType
+    return{
+      x: [chestPainType],
+      y: [CHOLESTEROL],
+      type: 'bar',
+      name: chestPainType
+    };
+  });
+  // Create the plot layout object
+  const LAYOUT = {
+    title: 'Average cholesterol rates by chest pain',
+    xaxis: {
+      title: 'Chest Pain Types'
+    },
+    yaxis: {
+      title: 'Average Cholesterol Rate'
+    },
+    legend: {
+      title: {
+        text: 'Chest Pain Types'
+      }
+    },
+    barmode: 'group'
   };
-});
-// Create the plot layout object
-const LAYOUT = {
-  title: 'Average cholesterol rates by chest pain',
-  xaxis: {
-    title: 'Chest Pain Types'
-  },
-  yaxis: {
-    title: 'Average Cholesterol Rate'
-  },
-  legend: {
-    title: {
-      text: 'Chest Pain Types'
-    }
-  },
-  barmode: 'group'
-};
 
-Plotly.newPlot('plot', TRACES,LAYOUT)
+  Plotly.newPlot('plot1', TRACES, LAYOUT);
+}
+
 
 ///////////////////////////////////////////////
 //       Resting ECG vs Max Heart Rate       //
 ///////////////////////////////////////////////
 function groupType(data) {
   // Group data by ageGroup
-  GROUPED_DATA = d3.group(data, d => d.ageGroup);
+  let GROUPED_DATA = d3.group(data, d => d.ageGroup);
   
   // Loop through the grouped data and create plot3 traces for each ageGroup
   const TRACES_PLOT3 = Array.from(GROUPED_DATA, ([ageGroup, groupData]) => {
